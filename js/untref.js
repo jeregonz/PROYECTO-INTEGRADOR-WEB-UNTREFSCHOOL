f6f85139f8e21b1b4b12f412826d4dcfa9713b19
Jeremias Gonzalez <jeregonza@hotmail.com.ar>
2023-07-20 16:39:53 -0300

    estilos en index  y contacto

---
+++ js/untref.js
@@ -0,0 +1,18 @@
+// let nav = document.querySelector("nav")
+// let btnMenu = document.getElementById("btnMenu");
+
+// btnMenu.addEventListener("click", toggleMenu);
+
+// function toggleMenu() {
+//     nav.classList.toggle("show");
+// }
+
+
+// nav {
+//     display: none;
+// }
+
+// nav.show {
+//     display: flex;
+//     flex-direction: column;
+// }
